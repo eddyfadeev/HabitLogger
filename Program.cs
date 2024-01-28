45c58766c5ac1f3157d9e799002082acad5ce243
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-01-28 00:43:20 -0700

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿namespace HabitLogger;
+﻿using HabitLoggerLibrary;
+
+namespace HabitLogger;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var dbase = new DatabaseManager("Data Source=HabitLogger.db;Version=3;");
+        
+        dbase.InitializeDatabase();
+        dbase.ExecuteQuery("INSERT INTO Habits (Name, Quantity, DateLogged) VALUES ('Test', 1, '2021-01-01')");
+        Console.WriteLine(dbase.ExecuteQuery("SELECT * FROM Habits"));
     }
 }
